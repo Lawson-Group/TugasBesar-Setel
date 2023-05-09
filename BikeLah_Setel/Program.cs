9eb75d58cd79aef8de49354ba9349e4d13e57404
novitangrh <novitasabila70@gmail.com>
2023-05-09 21:25:02 +0700

    Add Account Page

--- BikeLah_Setel/Program.cs
+++ BikeLah_Setel/Program.cs
@@ -16,7 +16,7 @@ namespace BikeLah_Setel
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Akun());
         }
     }
 }
